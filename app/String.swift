5d932a17900b237170f3f14bfb2d6750b00dcd84
David Kramer <davidkramer@me.com>
2018-12-12 21:39:59 -0800

    Refactor into multiple files

---
+++ app/String.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+// from https://stackoverflow.com/a/40040472/5829298
+extension String {
+    func matchingStrings(regex: String) -> [[String]] {
+        guard let regex = try? NSRegularExpression(pattern: regex, options: []) else { return [] }
+        let nsString = self as NSString
+        let results  = regex.matches(in: self, options: [], range: NSMakeRange(0, nsString.length))
+        return results.map { result in
+            (0..<result.numberOfRanges).map {
+                result.range(at: $0).location != NSNotFound
+                    ? nsString.substring(with: result.range(at: $0))
+                    : ""
+            }
+        }
+    }
+}
