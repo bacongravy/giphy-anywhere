265d55c86a05f797a03e77825ccba85c09727451
David Kramer <davidkramer@me.com>
2020-05-16 21:13:31 -0700

    Use fifo to import certificate data

--- scripts/setup_keychain.sh
+++ scripts/setup_keychain.sh
@@ -1,17 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
-CERTIFICATE_FILE=Certificate.p12
-KEYCHAIN_FILE=travis.keychain
-KEYCHAIN_PASSWORD=travis
+KEYCHAIN_FILE=default.keychain
+KEYCHAIN_PASSWORD=myvoiceismypassport
 
 echo "Setting up keychain..."
-
-echo $SIGNING_CERTIFICATE_P12_DATA | base64 --decode > $CERTIFICATE_FILE;
 
 security create-keychain -p $KEYCHAIN_PASSWORD $KEYCHAIN_FILE
 security default-keychain -s $KEYCHAIN_FILE
 security unlock-keychain -p $KEYCHAIN_PASSWORD $KEYCHAIN_FILE
-security import $CERTIFICATE_FILE -k $KEYCHAIN_FILE -P $SIGNING_CERTIFICATE_PASSWORD -T /usr/bin/codesign
+security import <(echo $SIGNING_CERTIFICATE_P12_DATA | base64 --decode) \
+                -f pkcs12 \
+                -k $KEYCHAIN_FILE \
+                -P $SIGNING_CERTIFICATE_PASSWORD \
+                -T /usr/bin/codesign
 
 # https://stackoverflow.com/a/40039594
 security set-key-partition-list -S apple-tool:,apple: -s -k $KEYCHAIN_PASSWORD $KEYCHAIN_FILE
